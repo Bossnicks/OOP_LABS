02dac7ea0f5d90aa7198e9a37b68921a4802dc3c
Nikon <nikon.chigoya@mail.ru>
2022-10-04 05:06:35 +0300

    first commit

---
+++ lab01/lab01/lab01/Program.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Text;
+
+namespace Lessons
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] arr = { "Яблоко", "от", "яблони", "недалеко", "падает" };
+            bool rrr = true;
+            int b = 1;
+            while (rrr)
+            {
+                Console.WriteLine("Выберите задание, которое хотите увидеть (1-Task1, 2-Task2, 3-Arrs, 4-Changer, 5-Jagged, 6-Cort, 0-Exit):");
+                string a = Console.ReadLine();
+                b = Convert.ToInt32(a);
+
+
+                switch (b)
+                {
+                    case 1:
+                        {
+                            Task1();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Task2();
+                            break;
+                        }
+                    case 3:
+                        {
+                            Arrs();
+                            break;
+                        }
+                    case 4:
+                        {
+                            Changer(arr);
+                            break;
+                        }
+                    case 5:
+                        {
+                            Jagged();
+                            break;
+                        }
+                    case 6:
+                        {
+                            Cort();
+                            break;
+                        }
+                    case 7:
+                        {
+                            int[] numForReturner = { 45, -65, 37, 0, -8, 19 };
+                            string strForReturner = "ASP.NET";
+                            (int max, int min, int sum, char firstLetter) = Returner(numForReturner, strForReturner);
+                            Console.WriteLine($"Максимальное значение элемента в массиве: {max}, минимальное: {min}, сумма всех элементов: {sum} \nпервая буква строки: {firstLetter}");
+                            break;
+                        }
+                    case 0:
+                        {
+                            rrr = false;
+                            break;
+                        }
+                }
+            }
+            //Task1();
+            //Task2();
+            //Arrs();
+            //Changer(arr);
+            //Jagged();
+            //Cort();
+        }
+        public static void Task1()
+        {
+            Console.WriteLine("Введите значения типа bool:");
+            bool trigger = System.Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Вот значения типа string явно преобразованное в bool:" + trigger);
+            byte num1 = 57;
+            Console.WriteLine("Значение типа byte:" + num1);
+            sbyte num2 = 21;
+            Console.WriteLine("Значение типа sbyte:" + num2);
+            Console.WriteLine("Введите значения типа char:");
+            char symb1 = (char)Console.Read();
+            Console.WriteLine("Значение типа char:" + symb1);
+            int num3 = 436278904;
+            Console.WriteLine("Значение типа int:" + num3);
+            decimal max = decimal.MaxValue;
+            Console.WriteLine("Значение типа decimal:" + max);
+            double num4 = 45384.3;
+            Console.WriteLine("Значение типа double:" + num4);
+            System.Int32 num5 = -234;
+            Console.WriteLine("Значение типа int:" + num5);
+            System.UInt32 num6 = 47237;
+            Console.WriteLine("Значение типа uint:" + num6);
+            nint num7 = nint.MinValue;
+            nuint num8 = nuint.MaxValue;
+            long num9 = long.MaxValue;
+            ulong num10 = ulong.MinValue;
+            short num11 = short.MaxValue;
+            ushort num12 = ushort.MinValue;
+            object setOfNumbers = (object)5432;
+            string str = 2345.ToString();
+            Console.WriteLine("Значение типа nint {0} и nuint {1}:", num7, num8);
+            Console.WriteLine("Значение типа long:" + num9);
+            Console.WriteLine("Значение типа ulong:" + num10);
+            Console.WriteLine("Значение типа short:" + num11);
+            Console.WriteLine("Значение типа ushort:" + num12);
+            Console.WriteLine("Пример объекта:" + setOfNumbers);
+            Console.WriteLine("Пример строки:" + str);
+            Console.WriteLine("Пример упаковки и распаковки:");
+            object o = num5;     // boxing
+            int j = (int)o;   // unboxing
+            var i = 5;
+            var i2 = i + '0';
+            Console.WriteLine("{0}      {1}        {2}", i, i2, i2.GetType());
+            int? n = null;
+            Console.WriteLine(n);
+            if (n == null)
+            {
+                Console.WriteLine("Здесь должен был быть нулевой тип");
+            }
+        }
+        public static void Task2()
+        {
+            string sent1 = "C# - объектно-ориентированный язык";
+            string sent2 = "Java - тоже объектно-ориентированный язык";
+            string sent3 = "А вот JavaScript применяет в основном функциональный подход";
+            int result = string.Compare(sent1, sent2);
+            string sent4 = string.Concat(sent3, ".");
+            string sent5 = string.Copy(sent4);
+            string sent6 = sent5.Substring(sent5.Length - 1);
+            string[] words = sent2.Split(' ');
+            sent3 = sent3.Insert(5, ",к примеру,");
+            sent2 = sent2.Remove(sent2.Length - 5);
+            string sent7 = $"{sent1} {sent4}";
+            Console.WriteLine(sent7);
+            string sent8 = "", sent9 = null, sent10 = " ";
+            String Test(string s)
+            {
+                if (String.IsNullOrEmpty(s))
+                    return "is null or empty";
+                else
+                    return String.Format("(\"{0}\") is neither null nor empty", s);
+            }
+            Console.WriteLine("String sent8 {0}.", Test(sent8));
+            Console.WriteLine("String sent9 {0}.", Test(sent9));
+            Console.WriteLine("String sent10 {0}.", Test(sent10));
+            string sent11 = string.Concat(sent8, sent9);
+            Console.WriteLine("String sent11 {0}.", Test(sent11));
+            StringBuilder sb = new StringBuilder("ABC", 50);
+            sb.Append(new char[] { 'D', 'E', 'F' });
+            sb.AppendFormat("GHI{0}{1}", 'J', 'k');
+            Console.WriteLine("{0} chars: {1}", sb.Length, sb.ToString());
+            sb.Insert(0, "Alphabet: ");
+            sb.Replace('k', 'K');
+            Console.WriteLine("{0} chars: {1}", sb.Length, sb.ToString());
+            sb.Remove(5, sb.Length - 10);
+            Console.WriteLine("{0} chars: {1}", sb.Length, sb.ToString());
+            sb.Insert(sb.Length, "LMNOP");
+            Console.WriteLine("{0} chars: {1}", sb.Length, sb.ToString());
+        }
+        static void Changer(string[] arr)
+        {
+            int maxForChecker = int.MaxValue;
+            try
+            {
+                checked
+                {
+                    maxForChecker++;
+                }
+            }
+            catch(OverflowException e) {
+                Console.WriteLine(e.Message);
+            }
+            Console.Write("Введите значнение, которое хотите изменить и новое значение:");
+            string word = Console.ReadLine();
+            string newWord = Console.ReadLine();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == word)
+                {
+                    arr[i] = newWord;
+                }
+            }
+            Console.WriteLine("Массив с изменённым значением:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.Write("\n");
+        }
+        static void Jagged()
+        {
+            int[][] jagged = new int[3][];
+            jagged[0] = new int[2];
+            jagged[1] = new int[3];
+            jagged[2] = new int[4];
+            Console.WriteLine("Введите значения для ступенчатого массива:");
+            for (int i = 0; i < jagged.Length; i++)
+            {
+                for (int j = 0; j < jagged[i].Length; j++)
+                {
+                    jagged[i][j] = int.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < jagged.Length; i++)
+            {
+                for (int j = 0; j < jagged[i].Length; j++)
+                {
+                    Console.Write(jagged[i][j] + " ");
+                }
+                Console.Write("\n");
+            }
+        }
+        static void Arrs()
+        {
+            Random rand = new Random();
+            int[,] multiDimensionalArray1 = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    multiDimensionalArray1[i, j] = rand.Next(1, 15);
+                    Console.Write(multiDimensionalArray1[i, j] + "  ");
+                    //Console.WriteLine();
+                }
+                Console.Write("\n");
+            }
+            string[] arr = { "Яблоко", "от", "яблони", "недалеко", "падает" };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.Write("\n");
+            Console.WriteLine($"Длина массива равна {arr.Length}");
+        }
+        static void Cort()
+        {
+            (int number, string stroka1, char symbol, string stroka2, ulong bigNumber) t =
+                (27, "Россия", 'R', "Cat", 446744073);
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Введите позицию элемента, который вы хотите вывести в консоль из кортежа(если хотите закончит, введите 0):");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        //t.number = Console.Read();
+                        Console.WriteLine(t.number);
+                        break;
+                    case 2:
+                        //t.stroka1 = Console.ReadLine();
+                        Console.WriteLine(t.stroka1);
+                        break;
+                    case 3:
+                        //t.symbol = Console.ReadKey().KeyChar;
+                        Console.WriteLine(t.symbol);
+                        break;
+                    case 4:
+                        //t.stroka2 = Console.ReadLine();
+                        Console.WriteLine(t.stroka2);
+                        break;
+                    case 5:
+                        //t.bigNumber = UInt64.Parse(Console.ReadLine());
+                        Console.WriteLine(t.bigNumber);
+                        break;
+                    case 0:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Неверное значение!");
+                        break;
+                }
+            }
+            (int number, string stroka1, char symbol, string stroka2, ulong bigNumber) = t;
+            var (numb, stro, _, _, bigNumb) = t;
+            (short, string, char, string, ulong) un = (27, "Россия", 'R', "Cat", 446744073);
+            var array = new object[2];
+            var str = "";
+            str = stro;
+            array[0] = numb;
+            array[1] = bigNumb;
+            Console.WriteLine(un == t);
+        }
+        static (int max, int min, int sum, char firstLetter) Returner(int[] arr, string str)
+        {
+            int minimum = int.MaxValue;
+            int maximum = int.MinValue;
+            int maxForChecker = int.MaxValue;
+            unchecked
+            {
+                maxForChecker++;
+            }
+            int sum = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] < minimum) minimum = arr[i];
+                if(arr[i] > maximum) maximum = arr[i];
+                sum += arr[i];
+            }
+            
+            return (maximum, minimum, sum, str[0]);
+        }
+    }
+}
---
+++ lab02/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace SimpleAlgorithmsApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections;
+    using global::SimpleAlgorithmsApp.SimpleAlgorithmsApp;
+    using System.Data.Common;
+
+    namespace SimpleAlgorithmsApp
+    {
+        
+        public class Node<T>
+        {
+            public Node(T data)
+            {
+                Data = data;
+            }
+            private Node() { }//закрытый конструктор
+            public T Data { get; private set; }
+            public Node<T> Next { get; set; }
+        }
+        public partial class NodeStack<T> : IEnumerable<T>, IEquatable<NodeStack<T>?>
+        {
+            static readonly DateTime baseline;//переменная только для чтения
+            const int variant = 14;//поле-константа
+            Node<T> head;
+            static int count;
+            public NodeStack()
+            {
+
+            }
+            static NodeStack()//статический конструктор
+            {
+                baseline = DateTime.Now;
+            }
+            public int Variant
+            {
+                get { return variant; }
+            }
+            public DateTime BaseLine
+            {
+                get { return baseline; }
+            }
+            public bool IsEmpty
+            {
+                get { return count == 0; }
+            }
+            public int Count
+            {
+                get { return count; }
+            }
+
+            public override bool Equals(object? obj)
+            {
+                return Equals(obj as NodeStack<T>);
+            }
+
+            public bool Equals(NodeStack<T>? other)
+            {
+                return other is not null &&
+                       EqualityComparer<Node<T>>.Default.Equals(head, other.head) &&
+                       Variant == other.Variant &&
+                       BaseLine == other.BaseLine &&
+                       IsEmpty == other.IsEmpty &&
+                       Count == other.Count &&
+                       identifier == other.identifier;
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(head, Variant, BaseLine, IsEmpty, Count, identifier);
+            }
+
+            public static bool operator ==(NodeStack<T>? left, NodeStack<T>? right)
+            {
+                return EqualityComparer<NodeStack<T>>.Default.Equals(left, right);
+            }
+
+            public static bool operator !=(NodeStack<T>? left, NodeStack<T>? right)
+            {
+                return !(left == right);
+            }
+        }
+        public partial class NodeStack<T> : IEnumerable<T>
+        {
+            public void Push(T item)
+            {
+                // увеличиваем стек
+                Node<T> node = new Node<T>(item);
+                node.Next = head; // переустанавливаем верхушку стека на новый элемент
+                head = node;
+                count++;
+            }
+            public T Pop()
+            {
+                // если стек пуст, выбрасываем исключение
+                if (IsEmpty)
+                    throw new InvalidOperationException("Стек пуст");
+                Node<T> temp = head;
+                head = head.Next; // переустанавливаем верхушку стека на следующий элемент
+                count--;
+                return temp.Data;
+            }
+            public T Peek()
+            {
+                if (IsEmpty)
+                    throw new InvalidOperationException("Стек пуст");
+                return head.Data;
+            }
+            public int identifier;
+            public int Changer(ref int id, float fl)
+            {
+                if(fl < 0)
+                {
+                    identifier = id;
+                    return identifier;
+                }
+                return -1;
+            }
+            
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return ((IEnumerable)this).GetEnumerator();
+            }
+
+            IEnumerator<T> IEnumerable<T>.GetEnumerator()
+            {
+                Node<T> current = head;
+                while (current != null)
+                {
+                    yield return current.Data;
+                    current = current.Next;
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            NodeStack<float> [] stack = new NodeStack<float>[3];
+            bool flagForTime = true;
+            for (int i = 0; i < 3; i++)
+            {
+                bool flag = true;
+                Console.WriteLine($"Заполните {i + 1}-ый стек:");
+                while (flag) {
+                    stack[i] = new NodeStack<float>();
+                    if (flagForTime)
+                    {
+                        Console.WriteLine($"Вариант лабораторной: {stack[i].Variant}; Текущее время: {stack[i].BaseLine}");
+                        flagForTime = false;
+                    }
+                    Console.WriteLine("Введите элемент:");
+                    float elementOfStack = float.Parse(Console.ReadLine());
+                    if (stack[i].Changer(ref i, elementOfStack) != -1)
+                    {
+                        Console.WriteLine($"Стек с номером {stack[i].identifier + 1} теперь имеет отрицательный элемент");
+                    }
+                    stack[i].Push(elementOfStack);
+                    Console.WriteLine("Хотите ввести ещё элемент\n(Введите 1 если да и 0 если нет):");
+                    int x = int.Parse(Console.ReadLine());
+                    flag = x == 1 ?  true : false;                     
+                }
+            }
+            int maxMean = 0, minMean = 0;
+            float max = float.MaxValue;
+            float min = float.MinValue;
+            for (int i = 0; i < 3; i++)
+            {
+                if(stack[i].Peek() > min)
+                {
+                    min = stack[i].Peek();
+                    maxMean = i;
+                }
+                if (stack[i].Peek() < max)
+                {
+                    max = stack[i].Peek();
+                    minMean = i;
+                }
+            }
+            Console.WriteLine($"Номер стека с наибольшим верхним элементом: {maxMean + 1}, с наименьшим: {minMean + 1}");
+            var user = new { Name = "Tom", Age = 34 };//анонимный тип
+            Console.WriteLine($"{user.Name}, {user.Age} года");
+
+            //for(int i = 0; i < 3; i++)
+            //{
+            //    if (stack[i].Changer(ref elementOfStack, out i) 
+            //}
+            //stack.Push(7.8f);
+            //stack.Push(0f);
+            //stack.Push(-3.4f);
+            //stack.Push(5f);
+
+            //foreach (var item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine();
+            //float header = stack.Peek();
+            //Console.WriteLine($"Верхушка стека: {header}");
+            //Console.WriteLine();
+            //Console.WriteLine($"Количество элементов в стеке {stack.Count}");
+            //header = stack.Pop();
+            //foreach (var item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine($"Количество элементов в стеке {stack.Count}");
+
+        }
+    }
+}
